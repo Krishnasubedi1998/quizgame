da8a6e1bc7fd67a17b8002c79ba4c0655a077d42
krishnasubedi1998 <al0731@unt.edu>
2021-04-21 22:30:10 -0500

    frontend project

---
+++ js/quiz.js
@@ -0,0 +1,47 @@
+const quiz = [
+  { question: `What food was the character "Pac-Man" modeled after?`, answer: `Pizza`,
+    choices: [`Pizza`, `Cheese`, `Cake`]
+  },
+  { question: `Blizzard Entertainment is most well known for what video game franchise?`, answer: `World of Warcraft`,
+    choices: [`World of Warcraft`, `Overwatch`, `Diablo`]
+  },
+  { question: `What is the best selling videogame to date? (2021)`, answer: `Minecraft`,
+    choices: [`Minecraft`, `Grand Theft Auto V`, `The Elder Scrolls V: Skyrim`]
+  },
+  { question: `What was the first video game character to have a balloon featured in the Macy’s Thanksgiving Day Parade? (1993)`, answer: `Sonic the Hedgehog`,
+    choices: [`Sonic the Hedgehog`, `Mario`, `Pac-Man`]
+  },
+  { question: `Which company is also responsible for the creation of popular dining franchise "Chuck E Cheese?"`, answer: `Atari`,
+    choices: [`Atari`, `505 Games`, `Frontier Developments`]
+  },
+  { question: `What is the most expensive video game made to date? (2021)`, answer: `Red Dead Redemption 2`,
+    choices: [`Red Dead Redemption 2`, `Cyberpunk 2077`, `Call of Duty: Modern Warfare 2`]
+  },
+  { question: `What year was the first virtual reality headset created?`, answer: `1995`,
+    choices: [`1995`, `2004`, `2012`]
+  },
+  { question: `What was the first commercially successful video game?`, answer: `Pong`,
+    choices: [`Pong`, `Tank`, `Tennis for Two`]
+  },
+  { question: `What is the highest-selling gaming console to date? (2021)`, answer: `PlayStation 2`,
+    choices: [`PlayStation 2`, `Wii`, `Xbox 360`]
+  },
+  { question: `What is the best-selling handheld gaming system to date? (2021)`, answer: `Nintendo DS`,
+    choices: [`Nintendo DS`, `Game Boy Advance`, `Game Boy`]
+  },
+  { question: `What ailment in children is Nintendo blamed the most for causing?`, answer: `Blistered thumbs`,
+    choices: [`Blistered thumbs`, `Loss of vision`, `Seizures`]
+  },
+  { question: `What is the highest-grossing movie based on a video game to date? (2021)`, answer: `Warcraft`,
+    choices: [`Warcraft`, `Detective Pikachu`, `Sonic the Hedgehog`]
+  },
+  { question: `What video game franchise has been sued for the most money?`, answer: `Grand Theft Auto`,
+    choices: [`Grand Theft Auto`, `Mortal Kombat`, `Donkey Kong`]
+  },
+  { question: `What United States president was asked (and declined) to voice the in-game president character in Fallout 3? (2008)`, answer: `Bill Clinton`,
+    choices: [`Bill Clinton`, `George W. Bush`, `George H.W. Bush`]
+  },
+  { question: `What world market generates the most revenue in the video game industry to date? (2021)`, answer: `China`,
+    choices: [`China`, `Japan`, `United States`]
+  }
+];
